7ea4d03e089e9df0a9ece01d32b2b88d50885a4d
Dhanuja Dissanayke <dhanujadisanayke@gmail.com>
2024-05-12 23:56:45 +0530

    Navbar.js file starting to code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
-        <NavBar />
+        <header>
+          <NavBar />
+        </header>
       </React.Fragment>
     </div>
   );
